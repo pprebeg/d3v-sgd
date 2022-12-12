ded344d51cff4ccba29326abeb212e84e4a40a46
gordankos <Gordan.Kos@mail.inet.hr>
2022-12-12 11:58:44 +0100

    Work in progress version of New HC GUI

--- commands/grillage_command.py
+++ commands/grillage_command.py
@@ -1,10 +1,13 @@
+import sys
+
+import PySide6.QtCore
 from PySide6.QtWidgets import QApplication, QMenu
 from PySide6.QtWidgets import QDialog, QPushButton,QGridLayout,QToolTip,QCheckBox,QComboBox
 from PySide6.QtWidgets import QTreeView,QMainWindow,QVBoxLayout,QHBoxLayout,QSizePolicy
 from PySide6.QtWidgets import QTreeWidget,QTreeWidgetItem,QDockWidget,QWidget,QGroupBox
-from PySide6.QtWidgets import QLabel, QSpinBox, QLineEdit, QDoubleSpinBox
+from PySide6.QtWidgets import QLabel, QSpinBox, QLineEdit, QDoubleSpinBox, QTableWidgetItem
 from PySide6.QtGui import QCursor
-
+from PySide6 import QtCore, QtGui, QtWidgets, QtUiTools
 
 import os
 from PySide6.QtCore import Slot,Qt,QPoint
@@ -112,6 +115,350 @@ def tmp_fun_gen_hc_var1():
     return hc_var_1
 
 
+# noinspection PyUnresolvedReferences
+class GenerateNewHC(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        loader = QtUiTools.QUiLoader()
+        ui_file = QtCore.QFile("../commands/hc_GUI.ui")  # Qt designer ui file
+        ui_file.open(QtCore.QFile.ReadOnly)
+        self.hc_gui = loader.load(ui_file, parent)
+        ui_file.close()
+        self.hc_gui.setWindowTitle("Generate New Grillage Structure")
+        self.hc_gui.show()
+
+        self.table_materials = self.hc_gui.tableMaterials
+        self.table_plate = self.hc_gui.tablePlate
+        self.table_beams = self.hc_gui.tableBeamProps
+        self.table_layouts = self.hc_gui.tableStiffenerLayouts
+
+        self.cbox_plate_mat = self.hc_gui.cBox_PlateMaterial
+        self.cbox_beam_mat = self.hc_gui.cBox_BeamMaterial
+        self.cbox_beam_type = self.hc_gui.cBox_BeamType
+        self.cbox_layout_beam = self.hc_gui.cBox_LayoutBeamProp
+        self.cbox_long_beam = self.hc_gui.cBox_LongitudinalBeamProp
+        self.cbox_tran_beam = self.hc_gui.cBox_TransverseBeamProp
+        self.cbox_stiff_beam = self.hc_gui.cBox_StiffenerBeamProp
+        self.cbox_layout_type = self.hc_gui.cBox_LayoutDefinitionType
+
+        self.cbox_beam_type.currentIndexChanged.connect(self.update_beam_combobox)
+
+        self.hc_gui.btnAddNewMaterial.clicked.connect(self.add_new_material_prop)
+        self.hc_gui.btnAddNewPlate.clicked.connect(self.add_new_plate_prop)
+        self.hc_gui.btnAddNewBeam.clicked.connect(self.add_new_beam)
+        self.hc_gui.btnAddNewLayout.clicked.connect(self.add_new_layout)
+        self.hc_gui.btnGenerateGrillage.clicked.connect(self.gnerate_new_hc)
+
+        self.load_materials_list(self.default_materials())
+        self.update_material_combobox()
+        self.update_beam_combobox()
+
+    def gnerate_new_hc(self):
+        # row_count = self.table_materials.rowCount()
+        # for row in range(0, row_count):
+        #     mat_id = int(self.table_materials.item(row, 0).text())
+        #     name = self.table_materials.item(row, 1).text()
+        #     E = float(self.table_materials.item(row, 2).text())
+        #     v = float(self.table_materials.item(row, 3).text())
+        #     Reh = float(self.table_materials.item(row, 4).text())
+        #     ro = float(self.table_materials.item(row, 5).text())
+        #
+        # return hc_variant
+        pass
+
+    @staticmethod
+    def default_materials():
+        def_mats = [{"ID": 1, "E": 210000, "v": 0.3, "ro": 7.85 * 10**(-9), "Reh": 235, "name": "ST24"},
+                    {"ID": 1, "E": 210000, "v": 0.3, "ro": 7.85 * 10**(-9), "Reh": 315, "name": "AH32"},
+                    {"ID": 3, "E": 210000, "v": 0.3, "ro": 7.85 * 10**(-9), "Reh": 355, "name": "AH36"}]
+        return def_mats
+
+    def load_materials_list(self, default_material_list):
+        """
+        Load default materials list into table_materials.
+        """
+        self.table_materials.setRowCount(3)
+        row_index = 0
+        for material in default_material_list:
+            ID = QTableWidgetItem(str(material["ID"]))
+            name = QTableWidgetItem(material["name"])
+            E = QTableWidgetItem(str(material["E"]))
+            v = QTableWidgetItem(str(material["v"]))
+            Reh = QTableWidgetItem(str(material["Reh"]))
+            ro = QTableWidgetItem(str(material["ro"]))
+
+            ID.setTextAlignment(Qt.AlignCenter)
+            name.setTextAlignment(Qt.AlignCenter)
+            E.setTextAlignment(Qt.AlignCenter)
+            v.setTextAlignment(Qt.AlignCenter)
+            Reh.setTextAlignment(Qt.AlignCenter)
+            ro.setTextAlignment(Qt.AlignCenter)
+
+            self.table_materials.setItem(row_index, 0, ID)
+            self.table_materials.setItem(row_index, 1, name)
+            self.table_materials.setItem(row_index, 2, E)
+            self.table_materials.setItem(row_index, 3, v)
+            self.table_materials.setItem(row_index, 4, Reh)
+            self.table_materials.setItem(row_index, 5, ro)
+            row_index += 1
+
+    def add_new_material_prop(self):
+        """
+        Add new material property to table_materials.
+        """
+        mat_ID_input = self.hc_gui.lineEdit_materialID.text()
+        name_input = self.hc_gui.lineEdit_materialName.text()
+        E_input = self.hc_gui.lineEdit_MaterialE.text()
+        v_input = self.hc_gui.lineEdit_Materialv.text()
+        Reh_input = self.hc_gui.lineEdit_MaterialReh.text()
+        ro_input = self.hc_gui.lineEdit_MaterialRo.text()
+
+        ID = QTableWidgetItem(mat_ID_input)
+        name = QTableWidgetItem(name_input)
+        E = QTableWidgetItem(E_input)
+        v = QTableWidgetItem(v_input)
+        Reh = QTableWidgetItem(Reh_input)
+        ro = QTableWidgetItem(ro_input)
+
+        ID.setTextAlignment(Qt.AlignCenter)
+        name.setTextAlignment(Qt.AlignCenter)
+        E.setTextAlignment(Qt.AlignCenter)
+        v.setTextAlignment(Qt.AlignCenter)
+        Reh.setTextAlignment(Qt.AlignCenter)
+        ro.setTextAlignment(Qt.AlignCenter)
+
+        row_count = self.table_materials.rowCount()
+        self.table_materials.insertRow(row_count)
+        self.table_materials.setItem(row_count, 0, ID)
+        self.table_materials.setItem(row_count, 1, name)
+        self.table_materials.setItem(row_count, 2, E)
+        self.table_materials.setItem(row_count, 3, v)
+        self.table_materials.setItem(row_count, 4, Reh)
+        self.table_materials.setItem(row_count, 5, ro)
+        self.update_material_combobox()
+
+    def get_materials_list(self):
+        """
+        :return: List of material names entered into table_materials.
+        """
+        row_count = self.table_materials.rowCount()
+        mat_list = [None] * row_count
+        for row in range(0, row_count):
+            name = self.table_materials.item(row, 1).text()
+            mat_list[row] = name
+        return mat_list
+
+    def update_material_combobox(self):
+        """
+        Updates material comboboxes for new plate and beam generation.
+        """
+        self.cbox_plate_mat.clear()
+        self.cbox_beam_mat.clear()
+        self.cbox_plate_mat.addItems(self.get_materials_list())
+        self.cbox_beam_mat.addItems(self.get_materials_list())
+
+    def update_stiffener_beam_combobox(self):
+        """
+        Updates stiffener beam combobox for new layout and grillage generation.
+        """
+        self.cbox_layout_beam.clear()
+        self.cbox_stiff_beam.clear()
+        self.cbox_layout_beam.addItems(self.get_beam_list())
+        self.cbox_stiff_beam.addItems(self.get_beam_list())
+
+    def add_new_plate_prop(self):
+        """
+        Add new plate property to table_plate.
+        """
+        plate_id_input = self.hc_gui.lineEdit_plateID.text()
+        plate_name_input = self.hc_gui.lineEdit_plateName.text()
+        tp_input = self.hc_gui.lineEdit_plateThickness.text()
+        plate_mat_input = self.cbox_plate_mat.currentText()
+
+        plate_id = QTableWidgetItem(plate_id_input)
+        plate_name = QTableWidgetItem(plate_name_input)
+        tp = QTableWidgetItem(tp_input)
+        plate_mat = QTableWidgetItem(plate_mat_input)
+
+        plate_id.setTextAlignment(Qt.AlignCenter)
+        plate_name.setTextAlignment(Qt.AlignCenter)
+        tp.setTextAlignment(Qt.AlignCenter)
+        plate_mat.setTextAlignment(Qt.AlignCenter)
+
+        row_count = self.table_plate.rowCount()
+        self.table_plate.insertRow(row_count)
+        self.table_plate.setItem(row_count, 0, plate_id)
+        self.table_plate.setItem(row_count, 1, plate_name)
+        self.table_plate.setItem(row_count, 2, tp)
+        self.table_plate.setItem(row_count, 3, plate_mat)
+
+    def get_T_beam_dims(self):
+        """
+        :return: T beam dimensions string.
+        """
+        hw = self.hc_gui.lineEdit_hw_T.text()
+        tw = self.hc_gui.lineEdit_tw_T.text()
+        bf = self.hc_gui.lineEdit_bf_T.text()
+        tf = self.hc_gui.lineEdit_tf_T.text()
+        dim_str = hw + "x" + tw + "/"
+        dim_str += bf + "x" + tf
+        return dim_str
+
+    def get_L_beam_dims(self):
+        """
+        :return: L beam dimensions string.
+        """
+        hw = self.hc_gui.lineEdit_hw_L.text()
+        tw = self.hc_gui.lineEdit_tw_L.text()
+        bf = self.hc_gui.lineEdit_bf_L.text()
+        tf = self.hc_gui.lineEdit_tf_L.text()
+        dim_str = hw + "x" + tw + "/"
+        dim_str += bf + "x" + tf
+        return dim_str
+
+    def get_FB_beam_dims(self):
+        """
+        :return: FB beam dimensions string.
+        """
+        hw = self.hc_gui.lineEdit_hw_FB.text()
+        tw = self.hc_gui.lineEdit_tw_FB.text()
+        dim_str = hw + "x" + tw
+        return dim_str
+
+    def get_Bulb_beam_dims(self):
+        """
+        :return: Bulb beam dimensions string.
+        """
+        hw = self.hc_gui.lineEdit_hw_Bulb.text()
+        tw = self.hc_gui.lineEdit_tw_Bulb.text()
+        dim_str = hw + "x" + tw
+        return dim_str
+
+    def get_Hat_beam_dims(self):
+        """
+        :return: L beam dimensions string.
+        """
+        h = self.hc_gui.lineEdit_h_Hat.text()
+        t = self.hc_gui.lineEdit_t_Hat.text()
+        bf = self.hc_gui.lineEdit_bf_Hat.text()
+        fi = self.hc_gui.lineEdit_fi_Hat.text()
+        dim_str = h + "x" + t + "x"
+        dim_str += bf + "x" + fi
+        return dim_str
+
+    def get_beam_dimensions(self):
+        beam_type = self.cbox_beam_type.currentText()
+        dim_str = None
+        if beam_type == "T":
+            dim_str = self.get_T_beam_dims()
+        elif beam_type == "L":
+            dim_str = self.get_L_beam_dims()
+        elif beam_type == "FB":
+            dim_str = self.get_FB_beam_dims()
+        elif beam_type == "Bulb":
+            dim_str = self.get_Bulb_beam_dims()
+        elif beam_type == "Hat":
+            dim_str = self.get_Hat_beam_dims()
+        return dim_str
+
+    def add_new_beam(self):
+        """
+        Add new beam property to table_beams.
+        """
+        beam_id_input = self.hc_gui.lineEdit_beamID.text()
+        beam_name_input = self.hc_gui.lineEdit_beamName.text()
+        beam_type_input = self.cbox_beam_type.currentText()
+        beam_dims_input = self.get_beam_dimensions()
+        beam_mat_input = self.cbox_beam_mat.currentText()
+
+        beam_id = QTableWidgetItem(beam_id_input)
+        beam_name = QTableWidgetItem(beam_name_input)
+        beam_type = QTableWidgetItem(beam_type_input)
+        beam_dims = QTableWidgetItem(beam_dims_input)
+        beam_mat = QTableWidgetItem(beam_mat_input)
+
+        beam_id.setTextAlignment(Qt.AlignCenter)
+        beam_name.setTextAlignment(Qt.AlignCenter)
+        beam_type.setTextAlignment(Qt.AlignCenter)
+        beam_dims.setTextAlignment(Qt.AlignCenter)
+        beam_mat.setTextAlignment(Qt.AlignCenter)
+
+        row_count = self.table_beams.rowCount()
+        self.table_beams.insertRow(row_count)
+        self.table_beams.setItem(row_count, 0, beam_id)
+        self.table_beams.setItem(row_count, 1, beam_name)
+        self.table_beams.setItem(row_count, 2, beam_type)
+        self.table_beams.setItem(row_count, 3, beam_dims)
+        self.table_beams.setItem(row_count, 4, beam_mat)
+        self.update_stiffener_beam_combobox()
+
+    def get_beam_list(self):
+        row_count = self.table_beams.rowCount()
+        beam_list = [None] * row_count
+        for row in range(0, row_count):
+            name = self.table_beams.item(row, 1).text()
+            beam_list[row] = name
+        return beam_list
+
+    def add_new_layout(self):
+        """
+        Add new stiffener layout to table_layouts.
+        """
+        layout_id_input = self.hc_gui.lineEdit_LayoutID.text()
+        layout_name_input = self.hc_gui.lineEdit_LayoutName.text()
+        layout_beam_input = self.cbox_layout_beam.currentText()
+        layout_type_input = self.cbox_layout_type.currentText()
+        layout_value_input = self.hc_gui.lineEdit_LayoutValue.text()
+
+        layout_id = QTableWidgetItem(layout_id_input)
+        layout_name = QTableWidgetItem(layout_name_input)
+        layout_beam = QTableWidgetItem(layout_beam_input)
+        layout_type = QTableWidgetItem(layout_type_input)
+        layout_value = QTableWidgetItem(layout_value_input)
+
+        layout_id.setTextAlignment(Qt.AlignCenter)
+        layout_name.setTextAlignment(Qt.AlignCenter)
+        layout_beam.setTextAlignment(Qt.AlignCenter)
+        layout_type.setTextAlignment(Qt.AlignCenter)
+        layout_value.setTextAlignment(Qt.AlignCenter)
+
+        row_count = self.table_layouts.rowCount()
+        self.table_layouts.insertRow(row_count)
+        self.table_layouts.setItem(row_count, 0, layout_id)
+        self.table_layouts.setItem(row_count, 1, layout_name)
+        self.table_layouts.setItem(row_count, 2, layout_beam)
+        self.table_layouts.setItem(row_count, 3, layout_type)
+        self.table_layouts.setItem(row_count, 4, layout_value)
+
+    def update_beam_combobox(self):
+        """
+        Hides all overlapping group boxes (gb) and shows the selected one.
+        """
+        beam_type = self.cbox_beam_type.currentText()
+        gb_T = self.hc_gui.groupBox_T_Scantlings
+        gb_L = self.hc_gui.groupBox_L_Scantlings
+        gb_FB = self.hc_gui.groupBox_FB_Scantlings
+        gb_Bulb = self.hc_gui.groupBox_Bulb_Scantlings
+        gb_Hat = self.hc_gui.groupBox_Hat_Scantlings
+
+        gb_T.hide()
+        gb_L.hide()
+        gb_FB.hide()
+        gb_Bulb.hide()
+        gb_Hat.hide()
+
+        if beam_type == "T":
+            gb_T.show()
+        elif beam_type == "L":
+            gb_L.show()
+        elif beam_type == "FB":
+            gb_FB.show()
+        elif beam_type == "Bulb":
+            gb_Bulb.show()
+        elif beam_type == "Hat":
+            gb_Hat.show()
+
+
 class SGDCommand(Command):
     def __init__(self):
         super().__init__()
@@ -134,7 +481,7 @@ class SGDCommand(Command):
         mb.addMenu(self.menuMain)
 
         actionNewHatch = self.menuModel.addAction("&New Hatch Cover")
-        actionNewHatch.triggered.connect(self.onNewHatchCover)
+        actionNewHatch.triggered.connect(self.onNewHatchCoverGUI)
 
         actionGenerateFEM = self.menuAnalysis.addAction("&Generate FEM...")
         actionGenerateFEM.triggered.connect(self.onGenerateFEM)
@@ -192,6 +539,9 @@ class SGDCommand(Command):
             manager.show_geometry([self._grillgeo])
         QApplication.restoreOverrideCursor()
 
+    def onNewHatchCoverGUI(self):
+        new_hc = GenerateNewHC(self.mainwin)
+        return new_hc
 
     @Slot()
     def onGeometryCreated(self, geometries: List[Geometry]):
